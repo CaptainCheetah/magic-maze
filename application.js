5034d8439d098955d6a6f79d754784590a4ac9a9
CaptainCheetah <30985844+CaptainCheetah@users.noreply.github.com>
2018-03-11 16:26:37 -0400

    Add files via upload

--- application.js
+++ application.js
@@ -2,29 +2,13 @@ var MAGICMAZE = {};
 
 // voice variables
 MAGICMAZE.SPEECH = {};
-// MAGICMAZE.SPEECH.repeatMsg = '';
-MAGICMAZE.SPEECH.talk = function(params){ // s, f, n, callback, callbackvar
-	console.log('MAGICMAZE.SPEECH.talk');
-	console.log(params);
-
-	// var wait = ((typeof params.w == 'undefined') ? 500 : params.w );
-
+MAGICMAZE.SPEECH.talk = function(params){
 	msg = new SpeechSynthesisUtterance();
 		msg.volume = 1; // 0 to 1
 		msg.rate = 1;
 		msg.pitch = 1;
 		msg.voice = MAGICMAZE.SPEECH.voice;
-
-
-/*
-		msg.onend = function(){
-			window.speechSynthesis.pause();
-			window.setTimeout(function() { window.speechSynthesis.resume() }, wait);
-			if (typeof params.callback != 'undefined') {
-				params.callback(params.callbackvar);
-			}
-		}
-*/
+	
 	msg.text = (( typeof params.s != 'undefined' ) ? params.s : '' );
 
 	console.log(msg);
@@ -35,14 +19,31 @@ MAGICMAZE.currentTimer = null;
 MAGICMAZE.interval = 180;
 MAGICMAZE.duration = 180;
 
+MAGICMAZE.setColorStatus = function(t){
+	var timeLeft = ((typeof t == 'number') ? t : 180);
+	var newClass = 'safe';
+	
+	if (timeLeft > 60){
+		newClass = 'safe';
+	} else if (timeLeft > 30) {
+		newClass = 'warning';
+	} else if (timeLeft > 10) {
+		newClass = 'danger';
+	} else {
+		newClass = 'critical';
+	}
+	$('body').removeClass().addClass(newClass);
+}
+
 MAGICMAZE.timer = function(params){
+		
 		window.speechSynthesis.cancel();
-		$('#timer').css('color','#FFF');
+		$('#timer').css('color','#000');
 		$('#flip').prop('disabled',false);
 		$('#start').prop('disabled',true);
 
 		MAGICMAZE.duration = ((typeof params != 'undefined' && typeof params.duration != 'undefined') ? params.duration : MAGICMAZE.interval);
-		document.getElementById("timer").innerHTML = ((MAGICMAZE.duration - (MAGICMAZE.duration % 60)) / 60).toString().padStart(2, '0') + ":" + (MAGICMAZE.duration % 60).toString().padStart(2, '0');
+		$('#timer').html(((MAGICMAZE.duration - (MAGICMAZE.duration % 60)) / 60).toString().padStart(2, '0') + ":" + (MAGICMAZE.duration % 60).toString().padStart(2, '0'));
 
 		MAGICMAZE.SPEECH.talk({'s':
 			(((MAGICMAZE.duration - (MAGICMAZE.duration % 60)) / 60) ? ((MAGICMAZE.duration - (MAGICMAZE.duration % 60)) / 60) + ' minutes': '' ) +
@@ -57,16 +58,18 @@ MAGICMAZE.timer = function(params){
 		}
 
 		MAGICMAZE.currentTimer = setInterval(function() {
-			// document.getElementById("timer").innerHTML = ((MAGICMAZE.duration - (MAGICMAZE.duration % 60)) / 60).toString().padStart(2, '0') + ":" + (MAGICMAZE.duration % 60).toString().padStart(2, '0');
+			MAGICMAZE.setColorStatus(MAGICMAZE.duration);
 			if (MAGICMAZE.duration == 0) {
+				window.speechSynthesis.cancel();
 				$('#flip').prop('disabled',true);
 				clearInterval(MAGICMAZE.currentTimer);
-				$('#timer').css('color','#F00');
+				// $('#timer').css('color','#F00');
 				MAGICMAZE.SPEECH.talk({'s': 'Game over'});
 			} else {
 				MAGICMAZE.duration--;
-				document.getElementById("timer").innerHTML = ((MAGICMAZE.duration - (MAGICMAZE.duration % 60)) / 60).toString().padStart(2, '0') + ":" + (MAGICMAZE.duration % 60).toString().padStart(2, '0');
+				$('#timer').html(((MAGICMAZE.duration - (MAGICMAZE.duration % 60)) / 60).toString().padStart(2, '0') + ":" + (MAGICMAZE.duration % 60).toString().padStart(2, '0'));
 				if ([30,20,10,5].indexOf(MAGICMAZE.duration) > -1){
+					window.speechSynthesis.cancel();
 					MAGICMAZE.SPEECH.talk({'s': MAGICMAZE.duration + ' seconds remaining'});
 				}
 			}
@@ -78,32 +81,36 @@ MAGICMAZE.flipTimer = function(){
 }
 MAGICMAZE.resetTimer = function(){
 	window.speechSynthesis.cancel();
-	$('#timer').css('color','#FFF');
+	$('body').removeClass().addClass('safe');
+	$('#timer').css('color','#000');
 	$('#flip').prop('disabled',true);
 	$('#start').prop('disabled',false);
 	clearInterval(MAGICMAZE.currentTimer);
 	MAGICMAZE.currentTimer = null;
 	MAGICMAZE.duration = MAGICMAZE.interval;
-	document.getElementById("timer").innerHTML = ((MAGICMAZE.duration - (MAGICMAZE.duration % 60)) / 60).toString().padStart(2, '0') + ":" + (MAGICMAZE.duration % 60).toString().padStart(2, '0');
+	$('#timer').html(((MAGICMAZE.duration - (MAGICMAZE.duration % 60)) / 60).toString().padStart(2, '0') + ":" + (MAGICMAZE.duration % 60).toString().padStart(2, '0'));
+}
+
+MAGICMAZE.initVoices = function(){
+	if(typeof window.speechSynthesis === 'undefined') {
+		return;
+	}
+	
+	$.each(window.speechSynthesis.getVoices(), function(idx, voice){
+		if (voice.name == 'Daniel' && voice.lang == 'en-GB'){ // 'Google UK English Male'
+			window.speechSynthesis.onvoiceschanged = false;
+			MAGICMAZE.SPEECH.voice = voice;
+			MAGICMAZE.SPEECH.talk({'s': 'Welcome to the Magic Maze mall; for all your adventuring needs!'});
+			return;
+		}
+	});
 }
 
 $(document).ready(function(){
-
-	window.speechSynthesis.onvoiceschanged = function() {
-
-		$.each(window.speechSynthesis.getVoices(), function(idx, voice){
-			// console.log(voice.name);
-			if (voice.name == 'Google UK English Female'){ // 'Google UK English Male'){
-				window.speechSynthesis.onvoiceschanged = false;
-				MAGICMAZE.SPEECH.voice = voice;
-				MAGICMAZE.SPEECH.talk({'s': 'Welcome to the Magic Maze mall; for all your dungeoneering needs!'});
-				// MAGICMAZE.timer();
-			}
-		});
-
-
+	MAGICMAZE.initVoices();
+	if (typeof window.speechSynthesis !== 'undefined' && window.speechSynthesis.onvoiceschanged !== undefined) {
+  		window.speechSynthesis.onvoiceschanged = MAGICMAZE.initVoices;
 	}
-
 	window.onbeforeunload = function(){
 		window.speechSynthesis.cancel();
 	}
